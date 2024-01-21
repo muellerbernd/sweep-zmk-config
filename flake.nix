f5701a82e34a2de29baec3dc4fec53ac995b872b
Bernd Müller <github@muellerbernd.de>
2024-01-21 14:33:44 +0100

    local build via nix WIP

--- flake.nix
+++ flake.nix
@@ -1,23 +1,49 @@
+# {
+#   description = "ZMK flake";
+#
+#   inputs = {
+#     flake-utils.url = "github:numtide/flake-utils";
+#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#   };
+#
+#   outputs = { flake-utils, nixpkgs, ... }:
+#     flake-utils.lib.eachDefaultSystem (system:
+#       let
+#         pkgs = import nixpkgs {
+#           inherit system;
+#         };
+#       in
+#       {
+#         devShell = pkgs.mkShell {
+#
+#           buildInputs = with pkgs; [
+#             # (python311Packages.pykwalify.overrideAttrs
+#             #   (old: {
+#             #     doCheck = false;
+#             #     pythonImportsCheck = [ ];
+#             #     nativeCheckInputs = [ ];
+#             #     disabledTests = [ ];
+#             #   }))
+#             python311Packages.west
+#           ];
+#         };
+#       });
+# }
 {
-  description = "ZMK flake";
+  description = "my project description";
 
+  # inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      {
-        devShell = nixpkgs.mkShell {
-
-          buildInputs = with nixpkgs; [
-            python311Packages.west
-          ];
-        };
-      });
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
 }
